3c5bfa2764fc56c4328a288f02b5aeae528d56d4
Milana Shamaeva <mllmomollm@gmail.com>
2021-10-03 23:58:38 +0500

    серый фон формы и произвольные позиции элементов формы

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -39,7 +39,7 @@ namespace new_rep
             // 
             this.button1.BackColor = System.Drawing.Color.Black;
             this.button1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.button1.Location = new System.Drawing.Point(284, 60);
+            this.button1.Location = new System.Drawing.Point(401, 236);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(67, 34);
             this.button1.TabIndex = 0;
@@ -52,7 +52,7 @@ namespace new_rep
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.Color.Black;
             this.label1.ForeColor = System.Drawing.SystemColors.ControlLight;
-            this.label1.Location = new System.Drawing.Point(12, 35);
+            this.label1.Location = new System.Drawing.Point(155, 53);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(284, 13);
             this.label1.TabIndex = 1;
@@ -61,6 +61,9 @@ namespace new_rep
             // 
             // textBox1
             // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.textBox1.Location = new System.Drawing.Point(15, 12);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(602, 20);
@@ -71,7 +74,7 @@ namespace new_rep
             // 
             this.button2.BackColor = System.Drawing.Color.Black;
             this.button2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.button2.Location = new System.Drawing.Point(499, 35);
+            this.button2.Location = new System.Drawing.Point(38, 122);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(103, 59);
             this.button2.TabIndex = 3;
@@ -83,8 +86,8 @@ namespace new_rep
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
-            this.ClientSize = new System.Drawing.Size(629, 94);
+            this.BackColor = System.Drawing.Color.Gray;
+            this.ClientSize = new System.Drawing.Size(629, 291);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
