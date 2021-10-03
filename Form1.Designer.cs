f47f37a35013d5d30d56af22630d6097accafabb
Milana Shamaeva <mllmomollm@gmail.com>
2021-10-03 23:15:32 +0500

    изменение цвета и размера формы

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -46,8 +46,8 @@ namespace new_rep
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.Black;
-            this.ClientSize = new System.Drawing.Size(184, 161);
+            this.BackColor = System.Drawing.Color.DimGray;
+            this.ClientSize = new System.Drawing.Size(120, 264);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "MILANA";
